36a8a84a228cba5093007ee5dc7f27f5d44aa10f
Filipe Monteiro <filipemonteirodev@gmail.com>
2026-04-19 23:30:27 -0300

    Add exercise 008 about on classiying positive and negative numbers.

---
+++ level_01/ex008.js
@@ -0,0 +1,9 @@
+// Ask for a number and tell if it is positive, negative or zero.
+
+function classifyNumber(number) {
+    if (number > 0) return "Positive";
+    if (number < 0) return "Negative"
+    return "Zero"
+}
+
+console.log(classifyNumber('-10'))        
