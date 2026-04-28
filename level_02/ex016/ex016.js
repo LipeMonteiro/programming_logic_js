3a99d9c20e620cd71f7997cde9e9d7d3537a25fd
Filipe Monteiro <filipemonteirodev@gmail.com>
2026-04-28 06:54:24 -0300

    Adds comments to exercise 016 for a better understanding

--- level_02/ex016/ex016.js
+++ level_02/ex016/ex016.js
@@ -7,17 +7,21 @@
 console.log(average(5,5,5,5,5)); */
 
 //Improved version using for
-let numbers = [];
 
-for(let i = 0; i < 5; i++){
-    numbers.push(Number(prompt("Enter a number:")))
+//Creates an empty array
+const numbers = [];
+
+//Creates a loop to insert 5 numbers into the array, using the push method
+for (let i = 0; i < 5; i++){
+    numbers.push(Number(prompt("Enter a number:")));
 }
 
-const average = (...nums) => {
-    let sum = 0;
+let average = (...nums) => { //...nums Means the received parameters will be grouped into an arrray called nums
+    let sum = 0; //Variable to accumulate to sums
     for(let i = 0; i < nums.length; i++){
-        sum += nums[i];
-    }
+        sum += nums[i]; //nums[i] Represents each item in the array
+    };
+    return sum / nums.length  //Average: Sum value divided by the number of array elements
 }
 
-console.log("Average:", average(...numbers))
+console.log(`Average: ${average(...numbers)}`) //Executes the function passing the array as a parameter
