82926be4508e52f8f0ee44177756e83132116327
Filipe Monteiro <filipemonteirodev@gmail.com>
2026-04-29 16:14:23 -0300

    Refactor exercise 017 to include efficient mathematical solutions for counting even numbers

--- level_02/ex017/ex017.js
+++ level_02/ex017/ex017.js
@@ -1,5 +1,7 @@
 //Count how many even numbers there are between 1 and 200.
 
+//Inefficient way, as there is a mathematical formula
+
 let evenNumbers = [];
 
 let evenBetween1and200 = () => {
@@ -12,3 +14,22 @@ let evenBetween1and200 = () => {
 } 
 
 console.log(evenBetween1and200());
+
+// Correct resolution using mathematical formula
+
+function returnEvens(x, y) {
+    return Math.floor((y / 2) - ((x - 1) / 2));
+}
+
+console.log(returnEvens(37, 843));
+
+//Logical Version (no ready-made formula)
+
+function countEvens(x, y){
+    let firstEven = x % 2 === 0 ? x : x + 1
+    let lastEven = y % 2 === 0 ? y : y - 1
+
+    return ((lastEven - firstEven) / 2) + 1
+}
+
+console.log(countEvens(37, 843))
